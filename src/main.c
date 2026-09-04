6893a6ae17bb02a7685af6ba58a8ad84856073c6
Rocco Stan <botmech@outlook.com>
2026-09-03 20:49:57 -0400

    added movment and stopping

--- src/main.c
+++ src/main.c
@@ -1,16 +1,37 @@
 #include <graphx.h>
 #include <stdlib.h>
 
+bool key, prevkey;
+key = kb_Data[1] == kb_2nd;
 
-int x = 0;
-int y = 0;
+bool key2, prevkey2;
+key2 = kb_Data[1] == kb_clear;
+
+bool running = true;
+
+int x = 160;
+int y = 1;
 
 /* Main function, called first */
 int main(void)
 {
-    gfx_Begin();
-    gfx_SetDrawBuffer();
-    gfx_FillRectangle(160, 120, 30, 30);
+    while (running) {
+        gfx_Begin();
+        gfx_SetDrawBuffer();
+    gfx_FillRectangle(x, y, 30, 30);
+
+
+    if (key && !prevkey) {
+    gfx_FillRectangle(x, y += 2, 30, 30);
+    }
+    prevkey = key;
+
+    if (key2 && !prevkey2) {
+        gfx_End();
+        running = false;
+    }
+    prevkey2 = key2;
+}
 
     return 0;
 }
