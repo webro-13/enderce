a0caa51cc5f75ac7b92aa5dd8bcd3544948da3a1
Rocco Stan <botmech@outlook.com>
2026-09-03 20:01:32 -0400

    draw something

--- src/main.c
+++ src/main.c
@@ -1,9 +1,16 @@
 #include <graphx.h>
 #include <stdlib.h>
 
+
+int x = 0;
+int y = 0;
+
 /* Main function, called first */
 int main(void)
 {
+    gfx_Begin();
+    gfx_SetDrawBuffer();
+    gfx_FillRectangle(160, 120, 30, 30);
 
     return 0;
 }
