8d5cd47797b88a664c902513f5dd9643f348ab9c
Rocco Stan <botmech@outlook.com>
2026-09-05 10:40:18 -0400

    add function for movement

--- src/main.cpp
+++ src/main.cpp
@@ -9,29 +9,8 @@ float playerY = 120;
 float velocityX = 0;
 float velocityY = 0;
 
-/* Main function, called first */
-int main(void)
-{
-
-    gfx_Begin();
-    gfx_SetDrawBuffer();
-
-    kb_SetMode(MODE_3_CONTINUOUS);
-
-    clock_t previousTime = clock();
-
-    while (!(kb_Data[6] & kb_Clear))
-    {
-
-    clock_t currentTime = clock();
-
-    clock_t elapsed = currentTime - previousTime;
-
-    float dt = static_cast<float>(elapsed) / CLOCKS_PER_SEC;
-
-    previousTime = currentTime;
-
-        if (kb_Data[7] & kb_Left)
+void updatePlayer(float dt){
+if (kb_Data[7] & kb_Left)
         {
             if (velocityX > -300.0f)
             {
@@ -132,7 +111,31 @@ int main(void)
             if (velocityY < 0)
                 velocityY = 0;
         }
+}
+
+/* Main function, called first */
+int main(void)
+{
+
+    gfx_Begin();
+    gfx_SetDrawBuffer();
+
+    kb_SetMode(MODE_3_CONTINUOUS);
+
+    clock_t previousTime = clock();
+
+    while (!(kb_Data[6] & kb_Clear))
+    {
+
+    clock_t currentTime = clock();
+
+    clock_t elapsed = currentTime - previousTime;
+
+    float dt = static_cast<float>(elapsed) / CLOCKS_PER_SEC;
+
+    previousTime = currentTime;
         
+    updatePlayer(dt);
 
         gfx_FillScreen(0);
         gfx_SetColor(255);
