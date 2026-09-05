9b78e68f084409236431e985e2b5b6bb9679101e
Rocco Stan <botmech@outlook.com>
2026-09-04 20:04:04 -0400

    add color

--- src/main.c
+++ src/main.c
@@ -13,11 +13,15 @@ int main(void)
     while (running) {
         gfx_Begin();
         gfx_SetDrawBuffer();
-    gfx_FillRectangle(x, y, 30, 30);
+        gfx_SetColor(0);
+gfx_FillRectangle(0, 0, 320, 240);
+        gfx_SetColor(255);
+        gfx_FillRectangle(x, y, 30, 30);
 
     kb_Scan();
 
-    if (kb_Data[7] & kb_2nd) {
+    if (kb_Data[7] & kb_Right) {
+    gfx_SetColor(255);
     gfx_FillRectangle(x, y += 2, 30, 30);
     }
 
